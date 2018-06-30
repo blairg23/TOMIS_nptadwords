6300e536c9fddd43cc7cf8785bab46806078bb99
aemil <mrnitrate@gmail.com>
2018-06-29 23:08:35 -0600

    Add unit tests for POST and PUT accounts

--- TOMIS_nptadwords/nptadwords/tests/test_views.py
+++ TOMIS_nptadwords/nptadwords/tests/test_views.py
@@ -90,3 +90,175 @@ class GetSingleAccountTest(TestCase):
         response = client.get(
             reverse('get_delete_update_account', kwargs={'pk': 30}))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class CreateNewAccountTest(TestCase):
+    """ Test module for inserting a new account """
+
+    def setUp(self):
+        self.valid_payload = {
+            "AccountDescriptiveName": "Nashville Pedal Tavern",
+            "CampaignId": "301791361",
+            "CampaignName": "Nashville Pedal Tavern",
+            "CampaignStatus": "enabled",
+            "CityCriteriaId": "1026083",
+            "CountryCriteriaId": "2840",
+            "CustomerDescriptiveName": "Nashville Pedal Tavern",
+            "ExternalCustomerId": "4807290630",
+            "IsTargetingLocation": "true",
+            "MetroCriteriaId": " --",
+            "MostSpecificCriteriaId": "9013184",
+            "RegionCriteriaId": "21175",
+            "Date": "2018-02-11",
+            "Device": "Computers",
+            "LocationType": "Physical location",
+            "AveragePosition": "1.3",
+            "Clicks": "1",
+            "Conversions": "0.00",
+            "ConversionValue": "0.00",
+            "Cost": "150000",
+            "Impressions": "4",
+            "Interactions": "1",
+            "InteractionTypes": "[\"Clicks\"]",
+            "VideoViews": "0"
+        }
+        self.invalid_payload = {
+            "AccountDescriptiveName": "",
+            "CampaignId": "301791361",
+            "CampaignName": "Nashville Pedal Tavern",
+            "CampaignStatus": "enabled",
+            "CityCriteriaId": "1026083",
+            "CountryCriteriaId": "2840",
+            "CustomerDescriptiveName": "Nashville Pedal Tavern",
+            "ExternalCustomerId": "4807290630",
+            "IsTargetingLocation": "true",
+            "MetroCriteriaId": " --",
+            "MostSpecificCriteriaId": "9013184",
+            "RegionCriteriaId": "21175",
+            "Date": "2018-02-11",
+            "Device": "Computers",
+            "LocationType": "Physical location",
+            "AveragePosition": "1.3",
+            "Clicks": "1",
+            "Conversions": "0.00",
+            "ConversionValue": "0.00",
+            "Cost": "150000",
+            "Impressions": "4",
+            "Interactions": "1",
+            "InteractionTypes": "[\"Clicks\"]",
+            "VideoViews": "0"
+        }
+
+    def test_create_valid_account(self):
+        response = client.post(
+            reverse('get_post_accounts'),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_invalid_account(self):
+        response = client.post(
+            reverse('get_post_accounts'),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+class UpdateSingleAccountTest(TestCase):
+    """ Test module for updating an existing account record """
+
+
+    def setUp(self):
+        self.test_account_1 = Account.objects.create(
+            AccountDescriptiveName='Test AccountDescriptiveName 1',
+            CampaignId=111111111, CampaignName='Test CampaignName 1',
+            CampaignStatus=True, CityCriteriaId=1111111,
+            CountryCriteriaId=1111, CustomerDescriptiveName='Test CustomerDescriptiveName 1',
+            ExternalCustomerId=1111111111, IsTargetingLocation=True,
+            MetroCriteriaId=' --', MostSpecificCriteriaId=1111111,
+            RegionCriteriaId=11111, Date='2018-06-27', Device='Computers',
+            LocationType='Physical Location', AveragePosition=1.3,
+            Clicks=1, Conversions=0.00, ConversionValue=0.00,
+            Cost=150000, Impressions=4, Interactions=1,
+            InteractionTypes='[\"Clicks\"]', VideoViews=0)
+        self.test_account_2 = Account.objects.create(
+            AccountDescriptiveName='Test AccountDescriptiveName 2',
+            CampaignId=222222222, CampaignName='Test CampaignName 2',
+            CampaignStatus=False, CityCriteriaId=2222222,
+            CountryCriteriaId=2222, CustomerDescriptiveName='Test CustomerDescriptiveName 2',
+            ExternalCustomerId=2222222222, IsTargetingLocation=False,
+            MetroCriteriaId=' --', MostSpecificCriteriaId=2222222,
+            RegionCriteriaId=22222, Date='2018-02-12', Device='Tablets with full browsers',
+            LocationType='Location of interest', AveragePosition=1.3,
+            Clicks=2, Conversions=4.00, ConversionValue=35.75,
+            Cost=170000, Impressions=8, Interactions=4,
+            InteractionTypes='[\"Clicks\"]', VideoViews=1)
+        self.valid_payload = {
+            "AccountDescriptiveName": "Nashville Pedal Tavern",
+            "CampaignId": "301791361",
+            "CampaignName": "Nashville Pedal Tavern",
+            "CampaignStatus": "enabled",
+            "CityCriteriaId": "1026083",
+            "CountryCriteriaId": "2840",
+            "CustomerDescriptiveName": "Nashville Pedal Tavern",
+            "ExternalCustomerId": "4807290630",
+            "IsTargetingLocation": "true",
+            "MetroCriteriaId": " --",
+            "MostSpecificCriteriaId": "9013184",
+            "RegionCriteriaId": "21175",
+            "Date": "2018-02-11",
+            "Device": "Computers",
+            "LocationType": "Physical location",
+            "AveragePosition": "1.3",
+            "Clicks": "1",
+            "Conversions": "0.00",
+            "ConversionValue": "0.00",
+            "Cost": "150000",
+            "Impressions": "4",
+            "Interactions": "1",
+            "InteractionTypes": "[\"Clicks\"]",
+            "VideoViews": "0"
+        }
+        self.invalid_payload = {
+            "AccountDescriptiveName": "",
+            "CampaignId": "301791361",
+            "CampaignName": "Nashville Pedal Tavern",
+            "CampaignStatus": "enabled",
+            "CityCriteriaId": "1026083",
+            "CountryCriteriaId": "2840",
+            "CustomerDescriptiveName": "Nashville Pedal Tavern",
+            "ExternalCustomerId": "4807290630",
+            "IsTargetingLocation": "true",
+            "MetroCriteriaId": " --",
+            "MostSpecificCriteriaId": "9013184",
+            "RegionCriteriaId": "21175",
+            "Date": "2018-02-11",
+            "Device": "Computers",
+            "LocationType": "Physical location",
+            "AveragePosition": "1.3",
+            "Clicks": "1",
+            "Conversions": "0.00",
+            "ConversionValue": "0.00",
+            "Cost": "150000",
+            "Impressions": "4",
+            "Interactions": "1",
+            "InteractionTypes": "[\"Clicks\"]",
+            "VideoViews": "0"
+        }
+
+    def test_valid_update_account(self):
+        response = client.put(
+            reverse('get_delete_update_account', kwargs={'pk': self.test_account_1.pk}),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_invalid_update_account(self):
+        response = client.put(
+            reverse('get_delete_update_account', kwargs={'pk': self.test_account_1.pk}),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
