4cb3c8b54234dc009984a87336442b43a6b47709
aemil <mrnitrate@gmail.com>
2018-07-01 20:29:48 -0600

    Move request data hanlding to function to improve code reuse.

--- TOMIS_nptadwords/nptadwords/views.py
+++ TOMIS_nptadwords/nptadwords/views.py
@@ -26,32 +26,7 @@ def get_delete_update_record(request, pk):
 
     # update detail of single record
     elif request.method == 'PUT':
-        data = {
-            'AccountDescriptiveName': request.data.get('AccountDescriptiveName'),
-            'CampaignId': int(request.data.get('CampaignId')),
-            'CampaignName': request.data.get('CampaignName'),
-            'CampaignStatus': bool(request.data.get('CampaignStatus') == 'enabled'),
-            'CityCriteriaId': criteria_id_cleaner(request.data.get('CityCriteriaId')),
-            'CountryCriteriaId': criteria_id_cleaner(request.data.get('CountryCriteriaId')),
-            'CustomerDescriptiveName': request.data.get('CustomerDescriptiveName'),
-            'ExternalCustomerId': int(request.data.get('ExternalCustomerId')),
-            'IsTargetingLocation': bool(request.data.get('IsTargetingLocation').lower() in ("yes", "true", "t", "1")),
-            'MetroCriteriaId': criteria_id_cleaner(request.data.get('MetroCriteriaId')),
-            'MostSpecificCriteriaId': criteria_id_cleaner(request.data.get('MostSpecificCriteriaId')),
-            'RegionCriteriaId': criteria_id_cleaner(request.data.get('RegionCriteriaId')),
-            'Date': datetime.strptime(request.data.get('Date'),"%Y-%m-%d").date(),
-            'Device': request.data.get('Device'),
-            'LocationType': request.data.get('LocationType'),
-            'AveragePosition': Decimal(request.data.get('AveragePosition')),
-            'Clicks': int(request.data.get('Clicks')),
-            'Conversions': Decimal(request.data.get('Conversions')),
-            'ConversionValue': Decimal(request.data.get('ConversionValue')),
-            'Cost': int(request.data.get('Cost')),
-            'Impressions': int(request.data.get('Impressions')),
-            'Interactions': int(request.data.get('Interactions')),
-            'InteractionTypes': request.data.get('InteractionTypes'),
-            'VideoViews': int(request.data.get('VideoViews')),
-        }
+        data = data_cleaner(request)
         serializer = RecordSerializers(data=data)
         if serializer.is_valid():
             serializer.save()
@@ -68,7 +43,16 @@ def get_post_records(request):
         return Response(serializer.data)
     # insert a new record for an record
     elif request.method == 'POST':
-        data = {
+        data = data_cleaner(request)
+        serializer = RecordSerializers(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+def data_cleaner(request):
+    return {
             'AccountDescriptiveName': request.data.get('AccountDescriptiveName'),
             'CampaignId': int(request.data.get('CampaignId')),
             'CampaignName': request.data.get('CampaignName'),
@@ -93,12 +77,7 @@ def get_post_records(request):
             'Interactions': int(request.data.get('Interactions')),
             'InteractionTypes': request.data.get('InteractionTypes'),
             'VideoViews': int(request.data.get('VideoViews')),
-        }
-        serializer = RecordSerializers(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    }
 
 
 def criteria_id_cleaner(id):
